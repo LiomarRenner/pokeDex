44fc415e5411277b2d42d24f6788aeb285d49d00
LiomarRenner <liomarrenner@gmail.com>
2021-06-03 17:44:24 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,27 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: 'https://pokeapi.co/api/v2',
 });
 
-export default api;
+const getPokemonList = () => {
+  api.get("/pokemon/")
+  .then((res) => {
+    console.log(res.data.results);
+  })
+  .catch((err) => {
+    console.log("Vixe, Meu padin teve um erro" + err)
+  })
+}
+
+const getPokemonInfo = () => {
+  api.get("/pokemon/name")
+  .then((res) => {
+    console.log(res.data.results);
+  })
+  .catch((err) => {
+    console.log("Vixe, Meu padin teve um erro" + err)
+  })
+}
+
+export { api, getPokemonList, getPokemonInfo };
