457182ff0d4d0e21665d178c08e572899d9925f3
LiomarRenner <liomarrenner@gmail.com>
2021-06-06 00:29:15 -0300

    Config files

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './routes/stackNavigator'
+import StackNavigator from './routes/stackNavigator';
 
 const Routes = () => (
   <NavigationContainer>
