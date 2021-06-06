04e8370e0eaf9aba0254385c59ebeefe42615e84
LiomarRenner <liomarrenner@gmail.com>
2021-06-06 11:06:13 -0300

    HomeScreen Update

--- src/pages/HomeScreen/index.tsx
+++ src/pages/HomeScreen/index.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   ImageBackground,
   StyleSheet,
   FlatList,
-  Image,
 } from 'react-native';
-import { useNavigation } from '@react-navigation/core';
 
 import POKEBALL from '../../assets/images/Pokeball.png';
 import PokemonCard from '../../components/PokemonCard';
@@ -16,49 +14,33 @@ const HomeScreen = () => {
 
   const { pokemonList, getPokemons } = usePokemonPaginated();
 
-  const navigation = useNavigation();
-
-  function handlePokeInfo(){
-    navigation.navigate('InfoPokeScreen');
-  }
-
-
   return (
     <>
       <ImageBackground
         source={POKEBALL}
-        style={styles.containerImage}
+        style={styles.containerImageBackground}
         resizeMode="cover"
       >
+        <FlatList
+          data = { pokemonList }
+          keyExtractor = { (pokemon) => pokemon.id}
+          renderItem = { ({ item:pokemon }) => ( <PokemonCard pokemon = { pokemon } /> )}
+          numColumns={ 1 }
+
+          onEndReached = { getPokemons }
+          onEndReachedThreshold = { 0.5 }
+        />
       </ImageBackground>
-
-      <FlatList
-        data = { pokemonList }
-        keyExtractor = { (pokemon) => pokemon.id}
-        renderItem = { ({ item }) => ( <Image source={{ uri: item.photo }} style={ styles.image }/> )}
-        numColumns={ 1 }
-
-        onEndReached = { getPokemons }
-        onEndReachedThreshold = { 0.5 }
-      />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  containerImage: {
+  containerImageBackground: {
     flex:1,
     justifyContent:'space-around',
     flexDirection:'column',
   },
-  containerCard: {
-
-  },
-  image: {
-    width: 50,
-    height: 50,
-  }
-
 })
 
 export default HomeScreen;
